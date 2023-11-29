81d203239306ce2c185e56bcfc28d3e9ceccb00c
auscay <ebowusifeanyi@gmail.com>
2023-11-29 15:31:47 +0100

    minor changes

--- resources/views/components/single-layout.blade.php
+++ resources/views/components/single-layout.blade.php
@@ -5,14 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <!-- Page Title -->
-        <title>Blog Single | Spyre - Slick contemporary multipurpose theme</title>
+        <title>Poulet Blog</title>
 
         <!-- Favicons -->
-        <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/logo.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/logo.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/logo.png">
         <link rel="manifest" href="../assets/img/favicons/site.webmanifest">
-        <link rel="mask-icon" href="../assets/img/favicons/safari-pinned-tab.svg" color="#f23838">
+        <link rel="mask-icon" href="../assets/img/logo.png" color="#f23838">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
 
